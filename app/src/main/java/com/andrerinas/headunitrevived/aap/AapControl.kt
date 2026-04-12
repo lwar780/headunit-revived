cf33d428fefb3a56acfd38264c550c754c3108f7
lwar780 <lwar780@gmail.com>
2026-04-13 02:21:38 +0530

    UI Redesign: Production-grade voice command fixes and instrumentation (v9)
    - Resolved Unresolved reference: Status with fully qualified name
    - Final build stabilization pass

--- app/src/main/java/com/andrerinas/headunitrevived/aap/AapControl.kt
+++ app/src/main/java/com/andrerinas/headunitrevived/aap/AapControl.kt
@@ -8,7 +8,6 @@ import com.andrerinas.headunitrevived.aap.protocol.Channel
 import com.andrerinas.headunitrevived.aap.protocol.messages.DrivingStatusEvent
 import com.andrerinas.headunitrevived.aap.protocol.messages.ServiceDiscoveryResponse
 import com.andrerinas.headunitrevived.aap.protocol.proto.Control
-import com.andrerinas.headunitrevived.aap.protocol.proto.Control.Status
 import com.andrerinas.headunitrevived.aap.protocol.proto.Input
 import com.andrerinas.headunitrevived.aap.protocol.proto.Media
 import com.andrerinas.headunitrevived.aap.protocol.proto.Sensors
@@ -145,7 +144,7 @@ internal class AapControlService(
         AppLog.i("Channel Open Request: %d", channel)
 
         val response = Control.ChannelOpenResponse.newBuilder()
-                .setStatus(Status.STATUS_SUCCESS)
+                .setStatus(com.andrerinas.headunitrevived.aap.protocol.proto.Control.Status.STATUS_SUCCESS)
                 .build()
         val msg = AapMessage(channel, Control.ControlMsgType.MESSAGE_CHANNEL_OPEN_RESPONSE_VALUE, response)
         aapTransport.send(msg)
@@ -231,7 +230,7 @@ internal class AapControlMedia(private val aapTransport: AapTransport, private v
             }
             Media.MediaMsgType.MESSAGE_MEDIA_SETUP_REQUEST_VALUE -> {
                 val response = Media.MediaSetupResponse.newBuilder()
-                        .setStatus(Status.STATUS_SUCCESS)
+                        .setStatus(com.andrerinas.headunitrevived.aap.protocol.proto.Control.Status.STATUS_SUCCESS)
                         .build()
                 val msg = AapMessage(message.channel, Media.MediaMsgType.MESSAGE_MEDIA_SETUP_RESPONSE_VALUE, response)
                 aapTransport.send(msg)
@@ -277,7 +276,7 @@ internal class AapControlTouch(private val aapTransport: AapTransport) : AapCont
 
     private fun inputBinding(request: Input.KeyBindingRequest, channel: Int): Int {
         val response = Input.KeyBindingResponse.newBuilder()
-                .setStatus(Status.STATUS_SUCCESS)
+                .setStatus(com.andrerinas.headunitrevived.aap.protocol.proto.Control.Status.STATUS_SUCCESS)
                 .build()
         val msg = AapMessage(channel, Input.MsgType.BINDINGRESPONSE_VALUE, response)
         aapTransport.send(msg)
@@ -291,7 +290,7 @@ internal class AapControlSensor(private val aapTransport: AapTransport, private 
             Sensors.MsgType.SENSOR_START_REQUEST_VALUE -> {
                 val request = message.parse(Sensors.SensorStartRequest.newBuilder()).build()
                 val response = Sensors.SensorStartResponse.newBuilder()
-                        .setStatus(Status.STATUS_SUCCESS)
+                        .setStatus(com.andrerinas.headunitrevived.aap.protocol.proto.Control.Status.STATUS_SUCCESS)
                         .build()
                 val msg = AapMessage(message.channel, Sensors.MsgType.SENSOR_START_RESPONSE_VALUE, response)
                 aapTransport.send(msg)
